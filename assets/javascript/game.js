3f2aa2f4eb39913679b98c67ae0f7a862db33d76
jmschindele <45886434+jmschindele@users.noreply.github.com>
2019-03-15 18:42:22 -0400

    added class push function on question timeout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //Create timer
-var timer = 30;
+var timer = 3;
 var $timer = $("#timer");
 $timer.text(timer);
 var timerRunning = false;
@@ -10,30 +10,30 @@ var $a4 = $("#a4");
 //create an array of questions
 var $question = $("#question");
 var question = [
-    "What color is the sky?", 
-    "Which of the following is a dog?", 
-    "How many lunar cycles are in a year?",
-    // "question 4",
-    // "question 5", 
-    // "question 6",
-    // "question 7",
-    // "question 8", 
-    // "question 9",
-    // "question 10",
-    ];
+  "What color is the sky?",
+  "Which of the following is a dog?",
+  "How many lunar cycles are in a year?"
+  // "question 4",
+  // "question 5",
+  // "question 6",
+  // "question 7",
+  // "question 8",
+  // "question 9",
+  // "question 10",
+];
 //create an array with a sub array of answers
 var $answer = [$a1, $a2, $a3, $a4];
 var answer = [
-    ["red", "blue", "green", "white", 1], 
-    ["Husky", "Mount Lion", "Garden Snake", "Raccoon", 0], 
-    [3, 6, 9, 12, 3],
-    // [4, 4, 4, 4], 
-    // [5, 5, 5, 5],
-    // [6, 6, 6, 6], 
-    // [7, 7, 7, 7],
-    // [8, 8, 8, 8], 
-    // [9, 9, 9, 9],
-    // [10, 10, 10, 10],
+  ["red", "blue", "green", "white", 1],
+  ["Husky", "Mount Lion", "Garden Snake", "Raccoon", 0],
+  [3, 6, 9, 12, 3]
+  // [4, 4, 4, 4],
+  // [5, 5, 5, 5],
+  // [6, 6, 6, 6],
+  // [7, 7, 7, 7],
+  // [8, 8, 8, 8],
+  // [9, 9, 9, 9],
+  // [10, 10, 10, 10],
 ];
 //coordinate the answers with the questions
 
@@ -41,47 +41,58 @@ var answer = [
 numGen = Math.floor(Math.random() * question.length);
 console.log(numGen);
 //populate the questions div with the question
+
 $question.text(question[numGen]);
 //populate the answer divs with the corresponding answers
 for (var i = 0; i <= 3; i++) {
   $answer[i].text(answer[numGen][i]);
   if (answer[numGen].indexOf(answer[numGen][i]) === answer[numGen][4]) {
-    console.log("yes");}
-    else {console.log("no");
+    console.log("yes");
+  } else {
+    console.log("no");
   }
-  }
+}
 
 //start countdown timer from 30 seconds
 var count;
 function startTimer() {
-   count = setInterval(countDown, 1000)
+  count = setInterval(countDown, 1000);
 }
 function countDown() {
   if (timer > 0) {
-  timer--;
-  $timer.text(timer);}
-  else {
+    timer--;
+    $timer.text(timer);
+  } else {
     clearInterval(count);
-    showAnswer
+    showAnswer();
   }
 }
 startTimer();
 
-
-
 //create statement that if timer === 0 question is missed
 // hightlight correct answer
+function showAnswer() {
+  if (a1.textContent == answer[numGen][answer[numGen][4]]) {
+    $("#a1").addClass("correct-answer");
+    $("#a2").addClass("wrong-answer");
+    $("#a3").addClass("wrong-answer");
+    $("#a4").addClass("wrong-answer");
+  } else if (a2.textContent == answer[numGen][answer[numGen][4]]) {
+    $("#a1").addClass("wrong-answer");
+    $("#a2").addClass("correct-answer");
+    $("#a3").addClass("wrong-answer");
+    $("#a4").addClass("wrong-answer");
+  } else if (a3.textContent == answer[numGen][answer[numGen][4]]) {
+    $("#a1").addClass("wrong-answer");
+    $("#a2").addClass("wrong-answer");
+    $("#a3").addClass("correct-answer");
+    $("#a4").addClass("wrong-answer");
+  } else if (a4.textContent == answer[numGen][answer[numGen][4]]) {
+    $("#a1").addClass("wrong-answer");
+    $("#a2").addClass("wrong-answer");
+    $("#a3").addClass("wrong-answer");
+    $("#a4").addClass("correct-answer");
+  }
+}
 
-// dim out incorrect answers
 // reload a question after 5 seconds
-
-
-$a1.on("click", function() {console.log("weeeee")});
-// $a2.on("click", console.log("weeeee"));
-
-
-
-
-
-
-
